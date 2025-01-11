541a361dc3d6b878e09ebf3f2078b1a02184f10e
masekere <masekeregift@gmail.com>
2025-01-11 19:07:43 +0200

    add Product model protected fillables

--- server/app/Models/Product.php
+++ server/app/Models/Product.php
@@ -9,4 +9,8 @@ class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'price', 'description', 'quantity'
+    ];
 }
